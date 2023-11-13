b2cd02479d26e4b1e4746c3ea0aa18ead5776cbc
ahmedmshakil1 <ahmedmshakil1@gmail.com>
2023-11-14 02:53:22 +0600

    added Controller

--- test-repo-java/src/mainPakage/FXMLDocumentController.java
+++ test-repo-java/src/mainPakage/FXMLDocumentController.java
@@ -9,7 +9,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 /**
  *
@@ -19,6 +21,12 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private Button buttonHolder;
+    @FXML
+    private RadioButton RadioButton;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -30,5 +38,9 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void buttonHolderonClick(ActionEvent event) {
+    }
     
 }
